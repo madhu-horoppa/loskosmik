23653ba9622d676a125ace0dc8d6e8b4e30da2eb
krisv <>
2015-12-25 17:20:37 +0530

    {/LOS/src/main/java/com/ps/los/Customer.java}

--- LOS/src/main/java/com/ps/los/Customer.java
+++ LOS/src/main/java/com/ps/los/Customer.java
@@ -5,28 +5,91 @@ package com.ps.los;
  */
 
 @javax.persistence.Entity
-public class Customer implements java.io.Serializable {
+public class Customer implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "CUSTOMER_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "CUSTOMER_ID_GENERATOR", sequenceName = "CUSTOMER_ID_SEQ")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "CUSTOMER_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(name = "CUSTOMER_ID_GENERATOR", sequenceName = "CUSTOMER_ID_SEQ")
+   private java.lang.Long id;
 
-    public Customer() {
-    }
-    
-    public Customer(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Name")
+   private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "Address")
+   private java.lang.String address;
+
+   @org.kie.api.definition.type.Label(value = "Age")
+   private java.lang.Integer age;
+
+   @org.kie.api.definition.type.Label(value = "Credit Score")
+   private java.lang.Integer creditscore;
+
+   public Customer()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.lang.String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(java.lang.String name)
+   {
+      this.name = name;
+   }
+
+   public java.lang.String getAddress()
+   {
+      return this.address;
+   }
+
+   public void setAddress(java.lang.String address)
+   {
+      this.address = address;
+   }
+
+   public java.lang.Integer getAge()
+   {
+      return this.age;
+   }
+
+   public void setAge(java.lang.Integer age)
+   {
+      this.age = age;
+   }
+
+   public java.lang.Integer getCreditscore()
+   {
+      return this.creditscore;
+   }
+
+   public void setCreditscore(java.lang.Integer creditscore)
+   {
+      this.creditscore = creditscore;
+   }
+
+   public Customer(java.lang.Long id, java.lang.String name,
+         java.lang.String address, java.lang.Integer age,
+         java.lang.Integer creditscore)
+   {
+      this.id = id;
+      this.name = name;
+      this.address = address;
+      this.age = age;
+      this.creditscore = creditscore;
+   }
 
 }
