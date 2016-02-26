bc3b2083c14dad2601e2802edfa53781771e052e
admin <>
2016-02-26 16:53:26 +0530

    {/LOSPOC/src/main/java/page/lospoc/FinancialDetails.java}

--- LOSPOC/src/main/java/page/lospoc/FinancialDetails.java
+++ LOSPOC/src/main/java/page/lospoc/FinancialDetails.java
@@ -41,6 +41,38 @@ public class FinancialDetails implements java.io.Serializable
 
    private Integer incomeCreditScore;
 
+   private java.lang.Integer cashAtBank;
+
+   private java.lang.Integer valueOfImmovableProperty;
+
+   private java.lang.Integer valueOfShareAndSecurities;
+
+   private java.lang.Integer fixedDeposits;
+
+   private java.lang.Integer currentBalanceInPf;
+
+   private java.lang.Integer others;
+
+   private java.lang.Integer totalAssests;
+
+   private java.lang.Integer creditSocietyLoan;
+
+   private java.lang.Integer employerLoan;
+
+   private java.lang.Integer homeLoan;
+
+   private java.lang.Integer pfLoan;
+
+   private java.lang.Integer vehicleLoan;
+
+   private java.lang.Integer personalLoan;
+
+   private java.lang.Integer otherLoan;
+
+   private java.lang.Integer creditCard;
+
+   private java.lang.Integer totalLiabilites;
+
    public FinancialDetails()
    {
    }
@@ -185,6 +217,168 @@ public class FinancialDetails implements java.io.Serializable
       this.incomeCreditScore = incomeCreditScore;
    }
 
+   public java.lang.Integer getCashAtBank()
+   {
+      return this.cashAtBank;
+   }
+
+   public void setCashAtBank(java.lang.Integer cashAtBank)
+   {
+      this.cashAtBank = cashAtBank;
+   }
+
+   public java.lang.Integer getValueOfImmovableProperty()
+   {
+      return this.valueOfImmovableProperty;
+   }
+
+   public void setValueOfImmovableProperty(
+         java.lang.Integer valueOfImmovableProperty)
+   {
+      this.valueOfImmovableProperty = valueOfImmovableProperty;
+   }
+
+   public java.lang.Integer getValueOfShareAndSecurities()
+   {
+      return this.valueOfShareAndSecurities;
+   }
+
+   public void setValueOfShareAndSecurities(
+         java.lang.Integer valueOfShareAndSecurities)
+   {
+      this.valueOfShareAndSecurities = valueOfShareAndSecurities;
+   }
+
+   public java.lang.Integer getFixedDeposits()
+   {
+      return this.fixedDeposits;
+   }
+
+   public void setFixedDeposits(java.lang.Integer fixedDeposits)
+   {
+      this.fixedDeposits = fixedDeposits;
+   }
+
+   public java.lang.Integer getCurrentBalanceInPf()
+   {
+      return this.currentBalanceInPf;
+   }
+
+   public void setCurrentBalanceInPf(java.lang.Integer currentBalanceInPf)
+   {
+      this.currentBalanceInPf = currentBalanceInPf;
+   }
+
+   public java.lang.Integer getOthers()
+   {
+      return this.others;
+   }
+
+   public void setOthers(java.lang.Integer others)
+   {
+      this.others = others;
+   }
+
+   public java.lang.Integer getTotalAssests()
+   {
+      return this.totalAssests;
+   }
+
+   public void setTotalAssests(java.lang.Integer totalAssests)
+   {
+      this.totalAssests = totalAssests;
+   }
+
+   public java.lang.Integer getCreditSocietyLoan()
+   {
+      return this.creditSocietyLoan;
+   }
+
+   public void setCreditSocietyLoan(java.lang.Integer creditSocietyLoan)
+   {
+      this.creditSocietyLoan = creditSocietyLoan;
+   }
+
+   public java.lang.Integer getEmployerLoan()
+   {
+      return this.employerLoan;
+   }
+
+   public void setEmployerLoan(java.lang.Integer employerLoan)
+   {
+      this.employerLoan = employerLoan;
+   }
+
+   public java.lang.Integer getHomeLoan()
+   {
+      return this.homeLoan;
+   }
+
+   public void setHomeLoan(java.lang.Integer homeLoan)
+   {
+      this.homeLoan = homeLoan;
+   }
+
+   public java.lang.Integer getPfLoan()
+   {
+      return this.pfLoan;
+   }
+
+   public void setPfLoan(java.lang.Integer pfLoan)
+   {
+      this.pfLoan = pfLoan;
+   }
+
+   public java.lang.Integer getVehicleLoan()
+   {
+      return this.vehicleLoan;
+   }
+
+   public void setVehicleLoan(java.lang.Integer vehicleLoan)
+   {
+      this.vehicleLoan = vehicleLoan;
+   }
+
+   public java.lang.Integer getPersonalLoan()
+   {
+      return this.personalLoan;
+   }
+
+   public void setPersonalLoan(java.lang.Integer personalLoan)
+   {
+      this.personalLoan = personalLoan;
+   }
+
+   public java.lang.Integer getOtherLoan()
+   {
+      return this.otherLoan;
+   }
+
+   public void setOtherLoan(java.lang.Integer otherLoan)
+   {
+      this.otherLoan = otherLoan;
+   }
+
+   public java.lang.Integer getCreditCard()
+   {
+      return this.creditCard;
+   }
+
+   public void setCreditCard(java.lang.Integer creditCard)
+   {
+      this.creditCard = creditCard;
+   }
+
+   public java.lang.Integer getTotalLiabilites()
+   {
+      return this.totalLiabilites;
+   }
+
+   public void setTotalLiabilites(java.lang.Integer totalLiabilites)
+   {
+      this.totalLiabilites = totalLiabilites;
+   }
+
    public FinancialDetails(java.lang.Long id, java.lang.Long processInstanceID,
          java.lang.String customerOccupation, java.lang.Integer month1Salary,
          java.lang.Integer month2Salary, java.lang.Integer month3Salary,
@@ -192,7 +386,16 @@ public class FinancialDetails implements java.io.Serializable
          java.lang.Integer year1Income, java.lang.Integer year3Income,
          java.lang.Integer avgAnnualIncome, java.lang.Integer otherIncome,
          java.lang.Integer existingMonthlyEMI,
-         java.lang.Integer incomeCreditScore)
+         java.lang.Integer incomeCreditScore, java.lang.Integer cashAtBank,
+         java.lang.Integer valueOfImmovableProperty,
+         java.lang.Integer valueOfShareAndSecurities,
+         java.lang.Integer fixedDeposits, java.lang.Integer currentBalanceInPf,
+         java.lang.Integer others, java.lang.Integer totalAssests,
+         java.lang.Integer creditSocietyLoan, java.lang.Integer employerLoan,
+         java.lang.Integer homeLoan, java.lang.Integer pfLoan,
+         java.lang.Integer vehicleLoan, java.lang.Integer personalLoan,
+         java.lang.Integer otherLoan, java.lang.Integer creditCard,
+         java.lang.Integer totalLiabilites)
    {
       this.id = id;
       this.processInstanceID = processInstanceID;
@@ -208,6 +411,22 @@ public class FinancialDetails implements java.io.Serializable
       this.otherIncome = otherIncome;
       this.existingMonthlyEMI = existingMonthlyEMI;
       this.incomeCreditScore = incomeCreditScore;
+      this.cashAtBank = cashAtBank;
+      this.valueOfImmovableProperty = valueOfImmovableProperty;
+      this.valueOfShareAndSecurities = valueOfShareAndSecurities;
+      this.fixedDeposits = fixedDeposits;
+      this.currentBalanceInPf = currentBalanceInPf;
+      this.others = others;
+      this.totalAssests = totalAssests;
+      this.creditSocietyLoan = creditSocietyLoan;
+      this.employerLoan = employerLoan;
+      this.homeLoan = homeLoan;
+      this.pfLoan = pfLoan;
+      this.vehicleLoan = vehicleLoan;
+      this.personalLoan = personalLoan;
+      this.otherLoan = otherLoan;
+      this.creditCard = creditCard;
+      this.totalLiabilites = totalLiabilites;
    }
 
 }
